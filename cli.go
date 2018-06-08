efe64bb9345e722a1f2e41eb9c0261ec280fcc1f
Tyler Boright <lupin3.ken@gmail.com>
2018-06-07 23:47:04 -0700

    adds description and stub function for the cli file

--- cli.go
+++ cli.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"github.com/fatih/color"
+	"github.com/urfave/cli"
+)
+
 func StartCli(args []string) (resp []string, err error) {
+	app := cli.NewApp()
+	app.Name = "scribe"
+	app.Version = "0.0.1"
+	app.Usage = "Quick and easy storage / retrieval of links from keywords"
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Tyler Boright",
+			Email: "ru.lai.development@gmail.com",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		color.Magenta("Add a link with scribe add <linkName> <link>")
+		return nil
+	}
+
+	app.Commands = []cli.Command{}
+
+	err = app.Run(args)
+	if err != nil {
+		return resp, err
+	}
+
 	return
 }
